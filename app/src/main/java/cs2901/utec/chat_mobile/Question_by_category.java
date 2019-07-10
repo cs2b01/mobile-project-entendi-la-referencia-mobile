74da3b1db7d07a88bd64d5d8504a9f01aeeb64e0
fabriziofranco <fabrizio.franco@utec.edu.peUt3c2675clearREADME.md it config --global user.email fabrizio.franco@utec.edu.pe>
2019-07-10 05:26:09 -0500

    Meets w/ team

--- app/src/main/java/cs2901/utec/chat_mobile/Question_by_category.java
+++ app/src/main/java/cs2901/utec/chat_mobile/Question_by_category.java
@@ -3,14 +3,12 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
-
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-
 import org.json.JSONException;
 import org.json.JSONObject;
 
